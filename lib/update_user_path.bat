50beea9b20c2c1e8277a4a784d400a2836014ea4
Maxime MOINEAU <maxime.moineau@free.fr>
2025-03-12 14:18:02 +0100

    Update update_user_path.bat

--- lib/update_user_path.bat
+++ lib/update_user_path.bat
@@ -2,26 +2,32 @@
 setlocal enabledelayedexpansion
 
 set /p paths=Enter paths to add to the system PATH:
-set /p envVars=Enter environment variable names and values (e.g., ""VAR1=value1;VAR2=value2""):
+set /p envVars=Enter environment variable names and values (e.g., VAR1 value1):
 
 echo The following changes will be made:
 echo Paths to add to the system PATH: !paths!
 echo Environment variables and their values: !envVars!
 
 echo Current PATH: %PATH%
+echo ========================
 
-set ""oldPath=%PATH%""
-set ""newPath=!oldPath;!paths!""
+set oldPath=%PATH%
+set newPath=!oldPath!;"!paths!"
 
+echo ========================
 echo Old PATH: !oldPath!
+echo ========================
 echo New PATH: !newPath!
 
 set /p confirm=Confirm changes? (Y/N):
 if /i ""!confirm!"" == ""Y"" (
-    setx PATH ""!newPath!""
-    for %%v in (!envVars!) do (
-        setx %%v
-    )
+    
+    echo setting up the env var...
+    setx !envVars!
+
+    echo setting up the paths...
+    setx PATH "!newPath!"
+
 ) else (
     echo Changes not made.
 )
